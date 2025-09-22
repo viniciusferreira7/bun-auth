bd8f416d61d8e0c4207e71c3fc5cf8fb8ad2d636
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-09-22 06:12:20 -0300

    docs: add openapi on project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
+import openapi from '@elysiajs/openapi';
 import { Elysia } from "elysia";
+import z from 'zod';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3333);
+const app = new Elysia().use(openapi({
+  mapJsonSchema: {
+    zod: z.toJSONSchema
+  }
+})) 
+.get("/", () => "Hello Elysia")
+.get('/users/:id', (request) => {
+  const { params } = request
+
+  console.log(params.id)
+}, {
+  detail: {
+    summary: 'Get user',
+    tags: ['users']
+  },
+  params: z.object({
+    id: z.uuid()
+  })
+})
+.listen(3333);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,

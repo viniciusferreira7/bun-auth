eaf383be3839b389a0aaa3c490ed10350a894db6
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-09-29 09:28:22 -0300

    chore: apply improvements into biome config

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,35 @@
 import openapi from '@elysiajs/openapi';
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 import z from 'zod';
 
-const app = new Elysia().use(openapi({
-  mapJsonSchema: {
-    zod: z.toJSONSchema
-  }
-})) 
-.get("/", () => "Hello Elysia")
-.get('/users/:id', (request) => {
-  const { params } = request
+const app = new Elysia()
+	.use(
+		openapi({
+			mapJsonSchema: {
+				zod: z.toJSONSchema,
+			},
+		})
+	)
+	.get('/', () => 'Hello Elysia')
+	.get(
+		'/users/:id',
+		(request) => {
+			const { params } = request;
 
-  console.log(params.id)
-}, {
-  detail: {
-    summary: 'Get user',
-    tags: ['users']
-  },
-  params: z.object({
-    id: z.uuid()
-  })
-})
-.listen(3333);
+			console.log(params.id);
+		},
+		{
+			detail: {
+				summary: 'Get user',
+				tags: ['users'],
+			},
+			params: z.object({
+				id: z.uuid(),
+			}),
+		}
+	)
+	.listen(3333);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );

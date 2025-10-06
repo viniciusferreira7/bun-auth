3cc1512fdab5134d70ef81d3bbfb7502fb2028ee
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-06 09:31:14 -0300

    feat: change generation of ids to uuid v7

--- src/db/schema/accounts.ts
+++ src/db/schema/accounts.ts
@@ -1,11 +1,14 @@
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { randomUUIDv7 } from 'bun';
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 import { users } from './users';
 
 export const accounts = pgTable('accounts', {
-	id: text('id').primaryKey(),
-	accountId: text('account_id').notNull(),
-	providerId: text('provider_id').notNull(),
-	userId: text('user_id')
+	id: uuid('id')
+		.primaryKey()
+		.$defaultFn(() => randomUUIDv7()),
+	accountId: uuid('account_id').notNull(),
+	providerId: uuid('provider_id').notNull(),
+	userId: uuid('user_id')
 		.notNull()
 		.references(() => users.id, { onDelete: 'cascade' }),
 	accessToken: text('access_token'),
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,5 +1,11 @@
+import { accounts } from './accounts';
+import { sessions } from './sessions';
 import { users } from './users';
+import { verifications } from './verifications';
 
 export const schema = {
 	users,
+	accounts,
+	verifications,
+	sessions,
 };
